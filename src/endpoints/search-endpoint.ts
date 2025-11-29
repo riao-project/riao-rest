1dfc8354d742084f72555d00b31175ce27fe2499
Drew Immerman <drewimmerman@gmail.com>
2025-11-29 16:49:08 -0500

    feat: overridable getQuery() on search endpoint

--- src/endpoints/search-endpoint.ts
+++ src/endpoints/search-endpoint.ts
@@ -55,14 +55,7 @@ export class RiaoSearchEndpoint<
 		},
 	});
 
-	override async handle(
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		request: ApiRequest,
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		response: ApiResponse,
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		next: ApiNextFunction
-	) {
+	protected async getQuery(request: ApiRequest): Promise<SelectQuery<T>> {
 		const limit = request.body['limit'] as number | undefined;
 		const offset = request.body['offset'] as number | undefined;
 		const orderBy = request.body['orderBy'] as keyof T | undefined;
@@ -81,6 +74,18 @@ export class RiaoSearchEndpoint<
 				Record<keyof T, 'ASC' | 'DESC'>
 			>;
 		}
+
+		return query;
+	}
+
+	override async handle(
+		request: ApiRequest,
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		response: ApiResponse,
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		next: ApiNextFunction
+	) {
+		const query = await this.getQuery(request);
 
 		const [records, count] = await Promise.all([
 			this.repo.find(query),
